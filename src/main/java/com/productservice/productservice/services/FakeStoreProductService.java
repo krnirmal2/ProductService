8ac8ec1217b62cdd7cc5ba751d05304baf022767
krnirmal2 <nirmalkrmajhi14@gmail.com>
2023-12-15 16:11:34 +0530

    spotless apply of jetbrain

--- src/main/java/com/productservice/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/productservice/productservice/services/FakeStoreProductService.java
@@ -8,10 +8,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service("fakeStoreProductService") // it means initialise the onject of ProductService
-public class FakeStoreProductService implements ProductService{
-    String getProductUrl = "https://fakestoreapi.com/products/1";
+public class FakeStoreProductService implements ProductService {
+    //    String getProductUrl = "https://fakestoreapi.com/products/1";
+    String getProductUrl = "https://fakestoreapi.com/products/{id}";// for variable id we can just use variable in curly
+    //braces
+
     private RestTemplateBuilder restTemplateBuilder;
     //create constructor for builder design pattern
+
+    RestTemplate restTemplate = restTemplateBuilder.build();//for rest Api get/post/delete option
 
 
     public FakeStoreProductService(RestTemplateBuilder restTemplateBuilder) {
@@ -22,25 +27,23 @@ public class FakeStoreProductService implements ProductService{
     public FakeStoreProductDtos getProductById(Long Id) {
         // RestTemplate
 
-        RestTemplate restTemplate = restTemplateBuilder.build();
         // get call put url , response, variable
-       ResponseEntity<FakeStoreProductDtos> responseEntity =  restTemplate.getForEntity(getProductUrl, FakeStoreProductDtos.class);
+        ResponseEntity<FakeStoreProductDtos> responseEntity = restTemplate.getForEntity(getProductUrl, FakeStoreProductDtos.class, Id);
 
 
 //       step2
-       // so as we get the fakeStore product dtos as more tight couple
+        // so as we get the fakeStore product dtos as more tight couple
         // to do it more loosly coupled lets  add one more extra abstract layer
         // so that next we can change what ever next requirement come in product
-       // add one more extra dtos for generic pupose and passed the product dtos
+        // add one more extra dtos for generic pupose and passed the product dtos
         FakeStoreProductDtos fakeStoreProductDtos = new FakeStoreProductDtos();
         convertFakeStoreProductDtoToGenericProductDtoForAbstractionLayer(fakeStoreProductDtos);
-
 
 
         return responseEntity.getBody();
     }
 
-    private static GenericProductDto convertFakeStoreProductDtoToGenericProductDtoForAbstractionLayer(FakeStoreProductDtos fakeStoreProductDtos){
+    private static GenericProductDto convertFakeStoreProductDtoToGenericProductDtoForAbstractionLayer(FakeStoreProductDtos fakeStoreProductDtos) {
         GenericProductDto genericProductDto = new GenericProductDto();
         genericProductDto.setId(fakeStoreProductDtos.getId());
         genericProductDto.setDescription(fakeStoreProductDtos.getDescription());
@@ -50,6 +53,7 @@ public class FakeStoreProductService implements ProductService{
         genericProductDto.setImage(fakeStoreProductDtos.getImage());
         return genericProductDto;
     }
+
     @Override
     public void getAllProduct() {
 
